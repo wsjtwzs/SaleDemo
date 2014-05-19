e366f5abb6a3962f022361924ca22ee3ea23167a
evafan2003 <evafan2003@HeFeideMac-mini.local>
2014-05-19 18:28:55 +0800

    add space

--- SaleDemo/mainViewController/MainViewController.m
+++ SaleDemo/mainViewController/MainViewController.m
@@ -40,11 +40,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-    
-    
-    
-    
-    
     switch (self.loginPerson) {
         case LoginPerson_saleman:
             [self.view addSubview:self.salemanView];
